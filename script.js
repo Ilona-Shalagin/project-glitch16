137fdaab8a3138e22a15ab572b336f6f89424d1e
Ilona  Shalagin <ilona.shalagin25@gmail.com>
2025-12-31 11:50:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const back = document.querySelector("#back");
 const next = document.querySelector("#next");
-const photos = ["images/new-york.jpeg","images/NY2.jpeg","images/NY3.jpeg","images/NY4.jpeg"];
+const photos = ["images/new-york.jpeg","images/NY2.jpeg","images/NY3.jpeg","images/NY4.jpeg","images/NY5.jpeg"];
 let i = 0;
 
 next.addEventListener("click",() => {
